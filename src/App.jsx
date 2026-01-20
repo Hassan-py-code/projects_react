815ca1ec9e6fafde750584092b0a4abb995b0510
Hassan_Achehrri <hachehri@gmail.com>
2026-01-20 14:29:03 +0000

    add logic to click any card and see

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@
 
 import {GameHeader} from "./components/GameHeader"
 
+
+
 import { Card } from "./components/Card";
+import { useEffect, useState } from "react";
 const cardValues = [
   "🍎",
   "🍌",
@@ -25,6 +28,50 @@ const cardValues = [
 
 
 function App(){
+     
+     const [cards, setCards]=useState([]);
+     const initilaizeGame=()=>{
+         // shaffle the cards
+        console.log(cardValues);
+        const finalCards = cardValues.map((value , index)=>({
+                
+                    id:index,
+                    value,
+                    isFlipped: false,
+                    isMatched:false,
+
+                
+        }));
+
+        setCards(finalCards);
+     };
+
+     useEffect(()=>{
+          initilaizeGame();
+     }, []);
+      
+     const handleCardClick= (card) => {
+         //don't allow clicking if card is alerady fllioing
+        if(card.isFlipped || card.isMatched){
+                return;
+          }
+        
+        //updade card flipped state
+
+        const newCards=cards.map((c)=>{
+            
+            if(c.id === card.id ){
+                
+                return{...c , isFlipped:true};
+            }else{
+                 return c;
+            }
+        });
+
+        setCards(newCards);
+        
+     }
+
 
      return(
          
@@ -32,7 +79,7 @@ function App(){
             <GameHeader score={3} moves={3}/>
             <div className="cards-grid">
                 {cardValues.map((card) =>(
-                    <Card card={card} />
+                    <Card card={card} onClick={handleCardClick} />
                 ))}
             </div>
         </div>
