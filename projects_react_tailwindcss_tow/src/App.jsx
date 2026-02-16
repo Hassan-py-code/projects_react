726c75d3bee03435f5b93a8039bcd25c278bc865
Hassan <hachehri@gmail.com>
2026-02-16 22:23:39 +0100

    add projects tow to started react and tailwind css

---
+++ projects_react_tailwindcss_tow/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+function App(){
+   return(
+         <>
+            
+           <div className="bg-gray-950 w-full overflow-hidden">
+              <div className="">
+                hddd
+              </div>
+           </div>
+
+         </>
+      )
+};
+
+
+export default App;
+
+
